a014ce3c2b85c665493ec44740b7885c2e2eee96
xwang0001 <XWANG@MICA.EDU>
2014-06-12 20:57:09 -0400

    drop down button

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
+$(function (){
+	var accordians = $('.accordian-content');
 
+	accordians.hide();
+	
+	$('.accordian-link').click(function (event) {
+		var target = $(this).attr('href');
+		
+		event.preventDefault();
+		
+		$(target).toggle();
+		
+	});
+});
